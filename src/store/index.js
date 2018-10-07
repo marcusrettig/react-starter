9af7df3f113b1526963e18b140a750ff2afb1cbc
Marcus Rettig <marcus0706@live.se>
2018-10-07 18:38:09 +0200

    Setup redux dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import counter from './counter';
 
 export default function configureStore() {
   const rootReducer = combineReducers({
     counter,
   });
-  return createStore(rootReducer);
+
+  let store;
+
+  if (process.env.NODE_ENV === 'production') {
+    store = createStore(rootReducer);
+  } else {
+    store = createStore(rootReducer, composeWithDevTools());
+  }
+
+  return store;
 }
